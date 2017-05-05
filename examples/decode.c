2acad772657796a2503da52572dcd67b18db95b2
Anonymous Maarten <anonymous.maarten@gmail.com>
2017-05-05 07:52:02 +0200

    Clean whitespaces in examples + bzero TGAData

--- examples/decode.c
+++ examples/decode.c
@@ -7,60 +7,53 @@
  *  the Free Software Foundation; either version 2 of the License, or
  *  (at your option) any later version.
  *
- *  Functions demonstrated: TGAOpen(), TGAReadImage(), TGAWriteImage(), 
+ *  Functions demonstrated: TGAOpen(), TGAReadImage(), TGAWriteImage(),
  *			    TGAClose()
  *
  */
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <strings.h>
 #include <tga.h>
 #include "utils.h"
 
 void make_header(TGA *src, TGA *dest)
 {
-	dest->hdr.id_len 	= src->hdr.id_len;
+	dest->hdr.id_len	= src->hdr.id_len;
 	dest->hdr.map_t		= src->hdr.map_t;
-	dest->hdr.img_t 	= src->hdr.img_t;
-	dest->hdr.map_first 	= src->hdr.map_first;
-	dest->hdr.map_entry 	= src->hdr.map_entry;
+	dest->hdr.img_t		= src->hdr.img_t;
+	dest->hdr.map_first	= src->hdr.map_first;
+	dest->hdr.map_entry	= src->hdr.map_entry;
 	dest->hdr.map_len	= src->hdr.map_len;
-	dest->hdr.x 		= src->hdr.x;
-	dest->hdr.y 		= src->hdr.y;
-	dest->hdr.width 	= src->hdr.width;
-	dest->hdr.height 	= src->hdr.height;
-	dest->hdr.depth 	= src->hdr.depth;
-	dest->hdr.vert 	        = src->hdr.vert;
-	dest->hdr.horz          = src->hdr.horz;
-	dest->hdr.alpha         = src->hdr.alpha;
+	dest->hdr.x		= src->hdr.x;
+	dest->hdr.y		= src->hdr.y;
+	dest->hdr.width		= src->hdr.width;
+	dest->hdr.height	= src->hdr.height;
+	dest->hdr.depth		= src->hdr.depth;
+	dest->hdr.vert		= src->hdr.vert;
+	dest->hdr.horz		= src->hdr.horz;
+	dest->hdr.alpha		= src->hdr.alpha;
 }
-
 
 int main(int argc, char *argv[])
 {
-       	TGA *in, *out;
-	TGAData *data;
-
 	if(argc != 3) {
 		fprintf(stderr, "Usage: %s INPUTFILE OUTPUTFILE\n", argv[0]);
 		return 1;
 	}
-	
-	data = (TGAData*)malloc(sizeof(TGAData));
-	if(!data) {
-		TGA_EXAMPLE_ERROR(TGAStrErrorCode(TGA_OOM));
-		return 1;
-	}
 
-        in = TGAOpen(argv[1], "r");
+	TGA *in = TGAOpen(argv[1], "r");
 	printf("[open] name=%s, mode=%s\n", argv[1], "r");
 
-	out = TGAOpen(argv[2], "w");
-        printf("[open] name=%s, mode=%s\n", argv[2], "w");
-	
+	TGA *out = TGAOpen(argv[2], "w");
+	printf("[open] name=%s, mode=%s\n", argv[2], "w");
 
-	data->flags = TGA_IMAGE_ID | TGA_IMAGE_DATA | TGA_RGB;
-	TGAReadImage(in, data);
+	TGAData data;
+	bzero(&data, sizeof(data));
+	data.flags = TGA_IMAGE_ID | TGA_IMAGE_DATA | TGA_RGB;
+
+	TGAReadImage(in, &data);
 	if (!TGA_SUCCEEDED(in)) {
 		TGA_EXAMPLE_ERROR(TGAStrError(in));
 		return 1;
@@ -68,20 +61,19 @@ int main(int argc, char *argv[])
 
 	make_header(in, out);
 
-	TGAWriteImage(out, data);
+	TGAWriteImage(out, &data);
 	if (!TGA_SUCCEEDED(out)) {
 		TGA_EXAMPLE_ERROR(TGAStrError(out));
 		return 1;
 	}
 
-        TGAFreeTGAData(data);
-        free(data);
+	TGAFreeTGAData(&data);
 
 	printf("[close]\n[close]\n");
-        TGAClose(in);
+	TGAClose(in);
 	TGAClose(out);
 
-        printf("[exit] main\n");
+	printf("[exit] main\n");
 
 	return 0;
 }
--- examples/tgadump.c
+++ examples/tgadump.c
@@ -10,8 +10,9 @@
  *  Functions demonstrated: TGAOpen(), TGAReadImage(), TGAClose()
  */
 
-#include <stdio.h>  
+#include <stdio.h>
 #include <stdlib.h>
+#include <strings.h>
 #include <tga.h>
 #include "utils.h"
 
@@ -25,62 +26,55 @@ char *imgt[7] = { "no image data included" , "uncompressed color mapped",
 
 int main(int argc, char *argv[])
 {
-        TGA *tga;
-	TGAData *data;
-        
 	if(argc != 2) {
 		fprintf(stderr, "Usage: %s INPUTFILE\n", argv[0]);
 		return 1;
 	}
 
-	data = (TGAData*)malloc(sizeof(TGAData));
-	if(!data) {
-		TGA_EXAMPLE_ERROR(TGAStrErrorCode(TGA_OOM));
-		return 1;
-	}
-	
-        printf("[open] name=%s, mode=%s\n", argv[1], "r");
-	tga = TGAOpen(argv[1], "r");
-	if (!TGA_SUCCEEDED(tga)) {
-		TGA_EXAMPLE_ERROR(TGAStrError(tga));
-		return 1;
-	}
-	
-	printf("[read] image\n");
-	data->flags = TGA_IMAGE_INFO;
-	TGAReadImage(tga, data);
+	printf("[open] name=%s, mode=%s\n", argv[1], "r");
+	TGA *tga = TGAOpen(argv[1], "r");
 	if (!TGA_SUCCEEDED(tga)) {
 		TGA_EXAMPLE_ERROR(TGAStrError(tga));
 		return 1;
 	}
 
-	if (data->flags & TGA_IMAGE_INFO) {
+	TGAData data;
+	bzero(&data, sizeof(data));
+	data.flags = TGA_IMAGE_INFO;
+
+	printf("[read] image\n");
+	TGAReadImage(tga, &data);
+	if (!TGA_SUCCEEDED(tga)) {
+		TGA_EXAMPLE_ERROR(TGAStrError(tga));
+		return 1;
+	}
+
+	if (data.flags & TGA_IMAGE_INFO) {
 		printf("[info] width=%i\n", tga->hdr.width);
 		printf("[info] height=%i\n", tga->hdr.height);
-		
+
 		printf("[info] color map type=%i\n", tga->hdr.map_t);
 		printf("-> [text] %s\n", cmap[tga->hdr.map_t]);
 
 		printf("[info] image type=%i\n", tga->hdr.img_t);
-		printf("-> [text] %s\n", (tga->hdr.img_t > 8) ? 
+		printf("-> [text] %s\n", (tga->hdr.img_t > 8) ?
 			imgt[tga->hdr.img_t - 5] : imgt[tga->hdr.img_t]);
 
 		printf("[info] depth=%i\n", tga->hdr.depth);
 		printf("[info] x=%i\n", tga->hdr.x);
 		printf("[info] y=%i\n", tga->hdr.y);
 			printf("[info] orientation=%s-%s\n",
-			       (tga->hdr.vert == TGA_BOTTOM) ?
-			       "bottom" : "top",
-		 	(tga->hdr.horz == TGA_LEFT) ?
-			       "left" : "right");
+				(tga->hdr.vert == TGA_BOTTOM) ?
+				"bottom" : "top",
+			(tga->hdr.horz == TGA_LEFT) ?
+				"left" : "right");
 	}
 
-        TGAFreeTGAData(data);
-        free(data);
+	TGAFreeTGAData(&data);
 
 	printf("[close]\n");
-        TGAClose(tga);
-        printf("[exit] main\n");
+	TGAClose(tga);
+	printf("[exit] main\n");
 
 	return 0;
 }
